568ba4750517acae787c53ead7645916d9b8900e
sravangorati2001 <sravangorati2001@gmail.com>
2024-05-18 21:11:18 -0700

    yml changes

--- trigger_training.py
+++ trigger_training.py
@@ -47,7 +47,7 @@ def main():
         image_uri=image_uri,
         role=role,
         instance_count=1,
-        instance_type='ml.m5.large',  # Valid instance type
+        instance_type='ml.p3.16xlarge',  # Valid instance type
         volume_size=30,
         max_run=3600,
         input_mode='File',
